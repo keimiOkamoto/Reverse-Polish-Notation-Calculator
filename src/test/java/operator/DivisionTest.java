ab7147be51a3812fdd9ebb14293c9633ca48d266
KeimiOkamoto <keimiokamoto@gmail.com>
2015-10-11 22:49:20 +0100

    Adds test method for multiplication operator.

--- src/test/java/operator/DivisionTest.java
+++ src/test/java/operator/DivisionTest.java
@@ -1,7 +1,14 @@
 package operator;
+import org.junit.Test;
+
 
 /**
  * Created by keimiokamoto on 11/10/2015.
  */
 public class DivisionTest {
+    @Test
+    public void shouldBeAbleToMultiplyValues() {
+
+    }
+
 }
